05285bce2446a7940a310faf6807196f464476c5
Win_9 <krrr883@gmail.com>
2022-07-05 01:00:09 +0900

    change Design

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -10,7 +10,7 @@ import javax.persistence.GeneratedValue;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
