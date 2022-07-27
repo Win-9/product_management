a90d002cd6561c612430a78e538af8f761ec7c52
Win_9 <krrr883@gmail.com>
2022-07-27 22:24:42 +0900

    add valid

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -9,9 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -30,7 +32,7 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm form, BindingResult result) {
+    public String create(@Valid MemberForm form, BindingResult result) {
 
         if (result.hasErrors()) {
             return "members/createMemberForm";
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -1,14 +1,18 @@
 package jpabook.jpashop.controller;
 
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class MemberForm {
 
+    @NotEmpty
     private String name;
     private String city;
     private String street;
